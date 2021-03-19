deb92d179ea95e22c672df0c0a9bc54b84d3a5e9
StevyMarlino <stevyabessolo@gmail.com>
2021-03-19 17:23:28 +0100

    add login v2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <div class="limiter">
-        <div class="container-login100" style="background-image: url('{{ asset('login/images/bg-01.jpg') }}');">
+        <div class="container-login100" style="background-image: url('{{ asset('login-page/images/bg-01.jpg') }}');">
             <div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					Account Login
 				</span>
-                <form class="login100-form validate-form p-b-33 p-t-5" method="POST" action="{{ route('') }}">
+                <form class="login100-form validate-form p-b-33 p-t-5" method="POST" action="{{ route('login') }}">
                     @csrf
                     <div class="wrap-input100 validate-input" data-validate = "Enter username">
-                        <input class="input100 @error('email') is-invalid @enderror" type="text" name="username" placeholder="User name">
+                        <input class="input100 @error('email') is-invalid @enderror" type="email" name="email" placeholder="Email">
                         <span class="focus-input100" data-placeholder="&#xe82a;"></span>
 
                         @error('email')
@@ -21,7 +21,7 @@
                     </div>
 
                     <div class="wrap-input100 validate-input" data-validate="Enter password">
-                        <input class="input100 @error('password') is-invalid @enderror" type="password" name="pass" placeholder="Password">
+                        <input class="input100 @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password">
                         <span class="focus-input100" data-placeholder="&#xe80f;"></span>
 
                         @error('password')
@@ -32,8 +32,8 @@
                     </div>
 
                     <div class="container-login100-form-btn m-t-32">
-                        <button class="login100-form-btn">
-                            Login
+                        <button type="submit" class="login100-form-btn">
+                            {{ __('Login') }}
                         </button>
                     </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,56 +10,112 @@
     <title>{{ config('app.name', 'Track Finance') }}</title>
 
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="{{ asset('login/images/icons/favicon.ico') }}"/>
+    <link rel="icon" type="image/png" href="{{ asset('login-page/images/icons/favicon.ico') }}"/>
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-page/vendor/bootstrap/css/bootstrap.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('login-page/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
+    <link rel="stylesheet" type="text/css"
+          href="{{ asset('login-page/fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-page/vendor/animate/animate.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-page/vendor/css-hamburgers/hamburgers.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/animsition/css/animsition.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-page/vendor/animsition/css/animsition.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-page/vendor/select2/select2.min.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/vendor/daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-page/vendor/daterangepicker/daterangepicker.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/css/util.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('login/css/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-page/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-page/css/main.css') }}">
     <!--===============================================================================================-->
 </head>
 <body>
 
-    <div id="app">
+<div id="app">
+{{--    <div id="loading">--}}
+{{--        <img id="loading-image" src="{{ asset('login-page/images/giphy.gif') }}" alt="Loading..."/>--}}
+{{--    </div>--}}
+    @auth
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Track Finance') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    @endauth
+
+    @yield('content')
 
 
-            @yield('content')
+</div>
 
+<div id="dropDownSelect1"></div>
 
-    </div>
+<!--===============================================================================================-->
+<script src="{{ asset('login-page/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('login-page/vendor/animsition/js/animsition.min.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('login-page/vendor/bootstrap/js/popper.js') }}"></script>
+<script src="{{ asset('login-page/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('login-page/vendor/select2/select2.min.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('login-page/vendor/daterangepicker/moment.min.js') }}"></script>
+<script src="{{ asset('login-page/vendor/daterangepicker/daterangepicker.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('login-page/vendor/countdowntime/countdowntime.js') }}"></script>
+<!--===============================================================================================-->
+<script src="{{ asset('login-page/js/main.js') }}"></script>
 
-    <div id="dropDownSelect1"></div>
+<script>
 
-    <!--===============================================================================================-->
-    <script src="{{ asset('login/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('login/vendor/animsition/js/animsition.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('login/vendor/bootstrap/js/popper.js') }}"></script>
-    <script src="{{ asset('login/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('login/vendor/select2/select2.min.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('login/vendor/daterangepicker/moment.min.js') }}"></script>
-    <script src="{{ asset('login/vendor/daterangepicker/daterangepicker.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('login/vendor/countdowntime/countdowntime.js') }}"></script>
-    <!--===============================================================================================-->
-    <script src="{{ asset('login/js/main.js') }}"></script>
+        $('#loading').load();
+
+</script>
 
 </body>
+
 </html>
