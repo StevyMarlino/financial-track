98c612d858a2de4b8e4b2c30c8161e7c9fb1724e
yvan-nguekam <nguekamyvan4@gmail.com>
2021-04-19 13:38:17 +0200

    qf

---
+++ resources/views/acceuil.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header class="bg-dark pt-9 pb-11 d-none d-md-block">
+        <div class="container-md">
+          <div class="row align-items-center">
+            <div class="col">
+
+              <!-- Heading -->
+              <h1 class="font-weight-bold text-white mb-2">
+                Account Settings
+              </h1>
+
+              <!-- Text -->
+              <p class="font-size-lg text-white-75 mb-0">
+                Settings for <a class="text-reset" href="mailto:dhgamache@gmail.com">dhgamache@gmail.com</a>
+              </p>
+
+            </div>
+            <div class="col-auto">
+
+              <!-- Button -->
+              <button class="btn btn-sm btn-gray-300-20 text-white">
+                Log Out
+              </button>
+
+            </div>
+          </div> <!-- / .row -->
+        </div> <!-- / .container -->
+      </header>
+
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+</body>
+</html>
