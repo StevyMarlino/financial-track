e9ec95488f768c5edb1d9440a0407a5180c92388
nyll <elfabnyl@gmail.com>
2021-06-15 14:27:49 +0100

    stuff

---
+++ app/Http/Controllers/VerificationController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class VerificationController extends Controller
+{
+    // //
+    // public function __construct()
+    // {
+    //     $this->middleware('guest')->except('logout');
+    // }
+
+    public function verify(Request $request){
+       
+       
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VerificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
+Route::post('/verify',[VerificationController::class, 'verify'])->name('check');
 
 /**
  *
