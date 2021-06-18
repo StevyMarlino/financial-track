a2c07b4a2fa688f027476dfe6d2502cb746cc23d
StevyMarlino <stevyabessolo@gmail.com>
2021-06-18 19:43:29 +0200

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -20,8 +20,8 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">Traffic</h5>
-                                <span class="h2 font-weight-bold mb-0">350,897</span>
+                                <h5 class="card-title text-uppercase text-muted mb-0">monthly recipes</h5>
+                                <span class="h2 font-weight-bold mb-0">{{ $sale_of_current_month }} XAF</span>
                             </div>
                             <div class="col-auto">
                                 <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
@@ -30,7 +30,11 @@
                             </div>
                         </div>
                         <p class="mt-3 mb-0 text-muted text-sm">
-                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                            @if($sale_of_current_month > $sale_of_last_month )
+                                <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> {{ $percent_of_recipes }} %</span>
+                            @else
+                                <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> {{ $percent_of_recipes }} %</span>
+                            @endif
                             <span class="text-nowrap">Since last month</span>
                         </p>
                     </div>
@@ -41,8 +45,8 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-                                <span class="h2 font-weight-bold mb-0">2,356</span>
+                                <h5 class="card-title text-uppercase text-muted mb-0">Verified Payments</h5>
+                                <span class="h2 font-weight-bold mb-0">{{ $domain_verify }}/ {{ $domain_paid }}</span>
                             </div>
                             <div class="col-auto">
                                 <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
@@ -51,8 +55,8 @@
                             </div>
                         </div>
                         <p class="mt-3 mb-0 text-muted text-sm">
-                            <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
-                            <span class="text-nowrap">Since last week</span>
+                            <span class="text-danger mr-2"></span>
+                            <span class="text-nowrap"></span>
                         </p>
                     </div>
                 </div>
